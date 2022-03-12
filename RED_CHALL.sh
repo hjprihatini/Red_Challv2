1cbf5b4aefa687b4b1c343ced088355afc679b31
REDSEC <68374733+hjprihatini@users.noreply.github.com>
2022-03-12 21:23:25 +0700

    Add files via upload

---
+++ RED_CHALL.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+clear
+toilet -F gay RED_CHALL
+echo
+echo "#############################"
+echo "Name : RedSec"
+echo "Program : Bash"
+echo "Youtube : https://youtube.com/RED SEC"
+echo "Team : Cysec || IXP || MXP || RT"
+echo "Versi : 1.0"
+echo "#############################"
+echo
+echo "[+] Ketik 1 untuk mulai"
+echo
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Input@Chall~# \e[0m\en' option
+echo
+echo "Proses......"
+sleep 3
+echo "Sukses Mengakses chall"
+echo
+echo "https://www.fooddrinktalk.com/article-details.php?id=4"
+sleep 2
+echo "http://www.brightbrothers.co.in/products.php?id=2"
+sleep 2
+echo "https://www.limaparkhotel.com/promos_.php?id=4"
+sleep 2
+echo "http://polyportables-id.com/products.php?ID=2"
+sleep 2
+echo "https://www.pudak-scientific.com/detail_products.php?id=26"
+sleep 2
+echo "http://p-mateindonesia.com/products.php?action=detail&ID=4"
+sleep 2
+echo "http://www.judoprincast.com/campeonato.php?id=186"
+sleep 2
+echo "http://www.pajtimi.com/faqebrenda.php?newsID=3"
+sleep 2
+echo "http://heritageaviation.in/news.php?id=2"
+sleep 2
+echo "http://www.knauf.co.th/newsdetail.php?newsId=9"
+sleep 2
+echo "http://rnamedical.com/products.php?id=2"
+echo
+sleep 1
+echo "MAKASIH UDAH DI PAKE TOOLS NYA"
+echo "NANTI SAYA AKAN UPDATE LAGI :)"
+echo
+exit
+
+
+
+
+
+
+
+
+
